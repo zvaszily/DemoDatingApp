bd4bc6237ef23c19563a935e346fa9ac631a9c85
zvaszily <zvaszily@gmail.com>
2022-11-10 16:43:41 +0100

    Initial commit

---
+++ app/src/main/java/hu/nye/demodatingapp/activity/MainActivity.kt
@@ -0,0 +1,14 @@
+package hu.nye.demodatingapp.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import hu.nye.demodatingapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
